e1684c9932129cd140a836715e80781840a5a1d9
fionicss <118982867+Fionicss@users.noreply.github.com>
2023-12-18 18:01:07 +0800

    Created password checking in js using keyup.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 let inputs = document.querySelectorAll("input");
+let pw = document.querySelector("#pw");
+let confirm = document.querySelector("#confirm");
+let button =document.querySelector("button");
+
 
 inputs.forEach (input => {
     input.addEventListener("invalid", e => {
-        input.classList.add("required")
-    })
-})
+        input.classList.add("required");
+    });
+});
+
+
+let check = function () {
+    if (pw.value != confirm.value) {
+        pw.classList.add ("error");
+        confirm.classList.add ("error");
+        console.log ("AAA")
+    } else {
+        console.log("Yes!");
+        pw.classList.remove ("error");
+        confirm.classList.remove ("error");
+    };
+};
